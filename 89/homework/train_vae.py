7f658108fa07f4a89ace6bf1e54efdafcbe5a68d
ArtyomLobanov <lobanov98@yandex.ru>
2018-11-16 20:22:49 +0300

    Implement VAE trainer

--- 89/homework/train_vae.py
+++ 89/homework/train_vae.py
@@ -1,6 +1,65 @@
+import argparse
+import logging
+import os
+import sys
+
+import torch
+import torchvision.datasets as datasets
+from torch.optim import Adam
+from torchvision import transforms
+
+from homework.vae.vae import VAE
+from homework.vae.vae import loss_function
+from homework.vae.trainer import Trainer
+
+
+def get_config():
+    parser = argparse.ArgumentParser(description='Training DCGAN on CIFAR10')
+
+    parser.add_argument('--log-root', type=str, default='../logs')
+    parser.add_argument('--data-root', type=str, default='data')
+    parser.add_argument('--log-name', type=str, default='train_dcgan.log')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='enables CUDA training')
+    parser.add_argument('--batch-size', type=int, default=500,
+                        help='input batch size for training')
+    parser.add_argument('--epochs', type=int, default=30,
+                        help='number of epochs to train ')
+    parser.add_argument('--n_show_samples', type=int, default=8)
+    parser.add_argument('--show_img_every', type=int, default=10)
+    parser.add_argument('--log_metrics_every', type=int, default=100)
+    config = parser.parse_args()
+    config.cuda = not config.no_cuda and torch.cuda.is_available()
+
+    return config
+
+
 def main():
-    # TODO your code here
-    pass
+    config = get_config()
+    logging.basicConfig(
+        format='%(asctime)s | %(message)s',
+        handlers=[
+            logging.FileHandler(os.path.join(config.log_root,
+                                             config.log_name)),
+            logging.StreamHandler()],
+        level=logging.INFO)
+
+    transform = transforms.Compose([transforms.ToTensor()])
+    train_dataset = datasets.MNIST(root=config.data_root, download=True,
+                               transform=transform, train=True)
+    test_dataset = datasets.MNIST(root=config.data_root, download=True,
+                                   transform=transform, train=False)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=config.batch_size, shuffle=True,
+                                             num_workers=4, pin_memory=True)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=config.batch_size,
+                                             num_workers=4, pin_memory=True)
+
+    model = VAE(image_size=28)
+
+    trainer = Trainer(model, train_loader, test_loader, Adam(model.parameters(), lr=0.0002, betas=(0.5, 0.999)),
+                      loss_function, device="cuda")
+
+    trainer.train(200, 60)
 
 
 if __name__ == '__main__':
--- 89/homework/vae/trainer.py
+++ 89/homework/vae/trainer.py
@@ -8,81 +8,93 @@ from tensorboardX import SummaryWriter
 
 class Trainer:
 
-    def __init__(self, model, train_loader, test_loader, optimizer,
-                 loss_function, device='cpu'):
+    def __init__(self, model, train_loader, test_loader, optimizer, loss_function, image_size=28, device='cpu'):
         self.model = model
+        model.to(device)
         self.train_loader = train_loader
         self.test_loader = test_loader
         self.optimizer = optimizer
         self.loss_function = loss_function
+        self.image_size=image_size
         self.device = device
         self.writer = SummaryWriter()
 
-    def train(self, epoch, log_interval):
+    def train(self, n_epoch, log_interval):
         self.model.train()
-        epoch_loss = 0
+        global_step = 0
+        for epoch in range(n_epoch):
+            epoch_loss = 0
 
-        for batch_idx, (data, _) in enumerate(self.train_loader):
-            # TODO your code here
-            train_loss = None
-            epoch_loss += train_loss
-            norm_train_loss = train_loss / len(data)
+            for batch_idx, (data, _) in enumerate(self.train_loader):
+                self.model.zero_grad()
+                data = data.to(self.device)
+                res, mu, logvar = self.model(data)
 
-            self.optimizer.step()
-            if batch_idx % log_interval == 0:
-                msg = 'Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                    epoch, batch_idx * len(data),
-                    len(self.train_loader.dataset),
-                    100. * batch_idx / len(self.train_loader),
-                    norm_train_loss)
-                logging.info(msg)
+                train_loss = self.loss_function(res, data, mu, logvar)
+                train_loss.backward()
 
-                batch_size = self.train_loader.batch_size
-                train_size = len(self.train_loader.dataset)
-                batches_per_epoch_train = train_size // batch_size
-                self.writer.add_scalar(tag='data/train_loss',
-                                       scalar_value=norm_train_loss,
-                                       global_step=batches_per_epoch_train * epoch + batch_idx)
+                epoch_loss += train_loss
+                norm_train_loss = train_loss / len(data)
 
-        epoch_loss /= len(self.train_loader.dataset)
-        logging.info(f'====> Epoch: {epoch} Average loss: {epoch_loss:.4f}')
-        self.writer.add_scalar(tag='data/train_epoch_loss',
-                               scalar_value=epoch_loss,
-                               global_step=epoch)
+                self.optimizer.step()
+                if batch_idx % log_interval == 0:
+                    msg = 'Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                        epoch, batch_idx * len(data),
+                        len(self.train_loader.dataset),
+                        100. * batch_idx / len(self.train_loader),
+                        norm_train_loss)
+                    logging.info(msg)
+                    self.writer.add_scalar(tag='data/train_loss',
+                                           scalar_value=norm_train_loss,
+                                           global_step=global_step)
+                    self.plot_generated(global_step, data, res, 'img/images')
+
+                global_step += 1
+
+            epoch_loss /= len(self.train_loader.dataset)
+            logging.info(f'====> Epoch: {epoch} Average loss: {epoch_loss:.4f}')
+            self.writer.add_scalar(tag='data/train_epoch_loss',
+                                   scalar_value=epoch_loss,
+                                   global_step=epoch)
 
     def test(self, epoch, batch_size, log_interval):
         self.model.eval()
         test_epoch_loss = 0
+        step = 0
 
         for batch_idx, (data, _) in enumerate(self.test_loader):
-            # TODO your code here
+            data = data.to(self.device)
+            res, mu, logvar = self.model(data)
 
-            test_loss = None
+            test_loss = self.loss_function(res, data, mu, logvar)
             test_epoch_loss += test_loss
 
             if batch_idx % log_interval == 0:
                 msg = 'Test Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                     epoch, batch_idx * len(data),
                     len(self.test_loader.dataset),
-                    100. * batch_idx / len(self.test_loader),
-                    test_loss / len(data))
+                           100. * batch_idx / len(self.test_loader),
+                           test_loss / len(data))
                 logging.info(msg)
 
                 batches_per_epoch_test = len(self.test_loader.dataset) // batch_size
                 self.writer.add_scalar(tag='data/test_loss',
                                        scalar_value=test_loss / len(data),
                                        global_step=batches_per_epoch_test * (epoch - 1) + batch_idx)
+                self.plot_generated(step, data, res, 'img/test_images')
+            step += 1
 
         test_epoch_loss /= len(self.test_loader.dataset)
         logging.info('====> Test set loss: {:.4f}'.format(test_epoch_loss))
         self.writer.add_scalar(tag='data/test_epoch_loss',
                                scalar_value=test_epoch_loss,
                                global_step=epoch)
-        self.plot_generated(epoch, batch_size)
 
-    def plot_generated(self, epoch, batch_size):
-        # TODO your code here
-        pass
+    def plot_generated(self, step_number, origin, generated, tag, image_count=10):
+        examples = torch.cat([origin[:image_count, :, :, :],
+                              generated[:image_count].view(image_count, 1, self.image_size, self.image_size)], dim=0)
+        image = vutils.make_grid(examples, scale_each=True, nrow=10)
+        self.writer.add_image(tag, image, step_number)
 
     def save(self, checkpoint_path):
         dir_name = os.path.dirname(checkpoint_path)
